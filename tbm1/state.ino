b816ab72f51bfd2f28e9e40ed623eebf2572f915
kyan18 <147436085+kyan18@users.noreply.github.com>
2025-01-15 17:41:58 -0800

    Update state.ino
    Cleaned up some of sasha's work and it compiles now.

--- tbm1/state.ino
+++ tbm1/state.ino
@@ -8,7 +8,7 @@ enum State {
   RUNNING, 
   ERROR, 
   STOP
-}
+};
 
 //sets current state to config 
 State currentState = CONFIG; 
@@ -25,50 +25,42 @@ const float adcResolution = 65536;
 void setup() {
   Serial.begin(9600);
   pinMode(thermocouplePin, INPUT); 
-  Serial.println("System initialized - Entering CONFIG state");
+  Serial.printf("System initialized - Entering CONFIG state");
+}
+  // start 
+void loop(){ 
   int adcIn = analogRead(thermocouplePin); 
   float calcVoltage = (adcIn * referenceVoltage) / adcResolution;
   float temp = (calcVoltage - 1.25) / 0.005;  // converts to °C (5 mV/°C) 
+  switch (currentState) { 
+    case CONFIG: 
+      Serial.printf("CONFIG: Configuring system"); // delay(3000) if needed 
+      currentState = RUNNING; 
+      break; 
 
-  // start 
-  void loop(){ 
-    switch (currentState) { 
-      case CONFIG: 
-        Serial.println("CONFIG: Configuring system"); // delay(3000) if needed 
-        currentState = RUNNING; 
-        break; 
+    case RUNNING:
+      Serial.printf("RUNNING - Current temp is %.1f°C\n", temp); 
+      if (temp >= maxTemp) {
+        Serial.printf("ERROR: Max Temp exceeded - Overheating detected"); 
+        currentState = ERROR; 
+      } 
+      break; 
 
-      case RUNNING:
-        Serial.println("RUNNING - Current temp is %.1f°C\n", temp); 
+    case ERROR: 
+      Serial.printf("Stopping system"); 
+      currentState = STOP; 
+      break; 
 
-        if (temp >= maxTemp) { 
-          Serial.println("ERROR: Max Temp exceeded - Overheating detected"); 
-          currentState = ERROR; 
-        } 
-        break; 
-
-      case ERROR: 
-        Serial.println("Stopping system"); 
-        currentState = STOP; 
-        break; 
-
-      case STOP:
-          // every time the system is stopped, it automatically resets. 
-          Serial.println("System stopped. Resetting system. "); 
-          currentState = CONFIG; 
-        break;  
-
-      default: 
-        Serial.println("Unknown state"); 
+    case STOP:
+        // every time the system is stopped, it automatically resets. 
+        Serial.printf("System stopped. Resetting system. "); 
         currentState = CONFIG; 
-        break; 
+      break;  
 
-
-    }
+    default: 
+      Serial.printf("Unknown state"); 
+      currentState = CONFIG; 
+      break; 
   }
+}
   
-
-
-
-
-
