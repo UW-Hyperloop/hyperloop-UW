94d991c477f65a611e0aaa8fc0058973f157a2c6
kyan18 <147436085+kyan18@users.noreply.github.com>
2025-01-15 17:50:59 -0800

    Update sensors.ino
    Cleaned up and compiles now. Tegbir is working on this though so it might all get replaced anyways lol

--- tbm1/sensors.ino
+++ tbm1/sensors.ino
@@ -1,32 +1,16 @@
 // Thermocouple code? - def has bugs and errors, but seems to be a good starting point 
+#define TC_PIN LED_out     // ADC pin being used?
+#define AREF 3.3           // voltage for esp32
+#define ADC_RESOLUTION 12 // Is the resolution 12-bit on the ESP32?  
+
 
 const int LED_out = 5; // what is the pin we use ? 
-
+float reading, voltage, temperature;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(LED_out, OUTPUT);
-
-}
-
-void loop() {
-#define TC_PIN LED_out     // ADC pin being used?
-#define AREF 3.3           // voltage for esp32
-#define ADC_RESOLUTION 12 // Is the resolution 12-bit on the ESP32?  
-
-float reading, voltage, temperature;
-
-float get_voltage(int raw_adc) {
-  return raw_adc * (AREF / (pow(2, ADC_RESOLUTION)-1));  
-}
-
-float get_temperature(float voltage) {
-  return (voltage - 1.25) / 0.005;
-}
-
-void setup() {
-  Serial.begin(9600);
 }
 
 void loop() {
@@ -38,4 +22,11 @@ void loop() {
   Serial.println(" C");
   delay(500);
 }
+
+float get_voltage(int raw_adc) {
+  return raw_adc * (AREF / (pow(2, ADC_RESOLUTION)-1));  
+}
+
+float get_temperature(float voltage) {
+  return (voltage - 1.25) / 0.005;
 }
