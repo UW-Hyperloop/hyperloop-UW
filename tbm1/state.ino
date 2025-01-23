31a7bb3fcee1740955de59bc3d0d10e15c83d56f
a-yankovs <150750157+a-yankovs@users.noreply.github.com>
2025-01-22 17:30:38 -0800

    Update state.ino

--- tbm1/state.ino
+++ tbm1/state.ino
@@ -1,5 +1,4 @@
-
-# include <Arduino.h>
+#include <Arduino.h>
 // option for code once we add more sensors: check states function returns the state - shd return a String 
 
 // defines system states 
@@ -8,9 +7,10 @@ enum State {
   RUNNING, 
   ERROR, 
   STOP
-}
+}; 
 
 //sets current state to config 
+// initaliser expected before currentState 
 State currentState = CONFIG; 
 
 // temp threshold 
@@ -23,41 +23,42 @@ const float adcResolution = 65536;
 void setup() {
   Serial.begin(9600);
   Serial.println("System initialized - Entering CONFIG state");
-
+}
   // start 
-  void loop(){ 
-    switch (currentState) { 
-      case CONFIG: 
-        Serial.println("CONFIG: Configuring system"); // delay(3000) if needed 
-        currentState = RUNNING; 
-        break; 
+void loop(){ 
+  switch (currentState) { 
+    case CONFIG: 
+      Serial.println("CONFIG: Configuring system"); // delay(3000) if needed 
+      currentState = RUNNING; 
+      break; 
 
-      case RUNNING:
-        Serial.println("RUNNING - Current temp is %.1f°C\n", temp); 
-        if (SystemData.motor_temp.value >= maxTemp || SystemData.estop_button.value == 1) { 
-          Serial.println("ERROR: Max Temp exceeded - Overheating detected"); 
-          currentState = ERROR; 
-        }
-        // else if - condition for other sensors : flow meter, bentonite sensor   
-        break; 
+    case RUNNING:
+      Serial.println("RUNNING - Current temp is %.1f°C\n", SystemData.motor_temp.value); 
+      if (SystemData.motor_temp.value >= maxTemp || SystemData.estop_button.value == 1) { 
+        Serial.println("ERROR: Max Temp exceeded - Overheating detected"); 
+        currentState = ERROR; 
+      }
+      // else if - condition for other sensors : flow meter, bentonite sensor   
+      break; 
 
-      case ERROR: 
-        Serial.println("Stopping system"); 
-        currentState = STOP; 
-        break; 
+    case ERROR: 
+      Serial.println("Stopping system"); 
+      currentState = STOP; 
+      break; 
 
-      case STOP:
-          // every time the system is stopped, it automatically resets. 
-          Serial.println("System stopped. Resetting system. "); 
-          currentState = CONFIG; 
-        break;  
-
-      default: 
-        Serial.println("Unknown state"); 
+    case STOP:
+        // every time the system is stopped, it automatically resets. 
+        Serial.println("System stopped. Resetting system. "); 
         currentState = CONFIG; 
-        break; 
+      break;  
 
+    default: 
+      Serial.println("Unknown state"); 
+      currentState = CONFIG; 
+      break; 
 
-    }
   }
 }
+
+  
+
